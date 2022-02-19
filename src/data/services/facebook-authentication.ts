a2aff6a0f85e8f6a0d75cfc003c66cf5d71bc09a
giuliano bortoloni <giulianoagria@hotmail.com>
2022-02-18 23:11:00 -0300

    refactor(facebook-authentication): move services

---
+++ src/data/services/facebook-authentication.ts
@@ -0,0 +1,14 @@
+import { LoadFacebookUserApi } from '@/data/contracts/api'
+import { AuthenticationError } from '@/domain/errors'
+import { FacebookAuthentication } from '@/domain/features'
+
+export class FacebookAuthenticationService {
+  constructor (
+    private readonly loadFacebookUserByTokenApi: LoadFacebookUserApi
+  ) {}
+
+  async perform (params: FacebookAuthentication.Params): Promise<AuthenticationError> {
+    await this.loadFacebookUserByTokenApi.loadUser(params)
+    return new AuthenticationError()
+  }
+}
