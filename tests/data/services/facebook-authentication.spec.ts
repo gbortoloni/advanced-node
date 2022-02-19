5008d199a11b552031e91a2f92020b7a8ca9e02d
giuliano bortoloni <giulianoagria@hotmail.com>
2022-02-18 23:04:21 -0300

    refactor(facebook-authentication): move facebookUserApi

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -1,3 +1,4 @@
+import { LoadFacebookUserApi } from '@/data/contracts/api'
 import { AuthenticationError } from '@/domain/errors'
 import { FacebookAuthentication } from '@/domain/features'
 
@@ -10,18 +11,6 @@ class FacebookAuthenticationService {
     await this.loadFacebookUserByTokenApi.loadUser(params)
     return new AuthenticationError()
   }
-}
-
-interface LoadFacebookUserApi {
-  loadUser: (params: LoadFacebookUserApi.Params) => Promise<LoadFacebookUserApi.Result>
-}
-
-namespace LoadFacebookUserApi {
-  export type Params = {
-    token: string
-  }
-
-  export type Result = undefined
 }
 
 class LoadFacebookUserApiSpy implements LoadFacebookUserApi {
